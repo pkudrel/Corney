d15f92bb380c1dded1e4d115e08a738ec9142177
Piotr Kudrel <piotr.kudrel@deneblab.com>
2018-01-15 19:29:52 +0100

    Fix file monitor

---
+++ src/eo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Start oe ");
+            System.Console.WriteLine("Args: " + string.Join("; ", args));
+            System.Console.ReadKey();
+        }
+    }
+}
